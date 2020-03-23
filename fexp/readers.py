5e3772dd015891b275b37501b4f42ba41380b60b
Jonas Teuwen <jonasteuwen@gmail.com>
2020-03-23 18:32:26 +0100

    Width tag was not properly passed

--- fexp/readers.py
+++ fexp/readers.py
@@ -138,7 +138,7 @@ def read_mammogram(filename, dtype=np.int):
     """
     extra_tags = [DICOM_MODALITY_TAG, _DICOM_VOI_LUT_FUNCTION,
                   _DICOM_LATERALITY, _DICOM_IMAGE_LATERALITY,
-                  _DICOM_WINDOW_CENTER_TAG, _DICOM_WINDOW_CENTER_TAG,
+                  _DICOM_WINDOW_WIDTH_TAG, _DICOM_WINDOW_CENTER_TAG,
                   _DICOM_FIELD_OF_VIEW_HORIZONTAL_FLIP, _DICOM_PATIENT_ORIENTATION,
                   _DICOM_PHOTOMETRIC_INTERPRETATION]
 
