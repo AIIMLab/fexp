7dd25b3120232254e1d8cdf99908c9dfac00df23
RaySheombarsing <R.S.S.Sheombarsing@outlook.com>
2020-05-01 13:15:53 +0200

    deleted everything except affine transformation

--- fexp/augmentation/transforms.py
+++ fexp/augmentation/transforms.py
@@ -1,11 +1,14 @@
-# encoding: utf-8
-__author__ = 'Ray Sheombarsing'
+"""
+Copyright (c) Fexp Contributors
+
+This source code is licensed under the MIT license found in the
+LICENSE file in the root directory of this source tree.
+"""
 
 # Module for applying (deterministic) transformations to (2d) images.
 
 import torch
 import torch.nn.functional as nnf
-from augmentation import convolution
 
 
 def compute_grid(discret_size):
@@ -103,98 +106,3 @@ def affine_transformation(image, lin_transform, shift=torch.zeros(2, 1), origin=
 
     # Flip image back to usual "upside-down" convention
     return transformed_image[0, :, torch.arange(discret_size[0] - 1, -1, -1), :]
-
-
-def flip(image, axis):
-    """
-    Flip 2d image along prescribed axis with given probability. Implementation is specific to 2d setting to avoid
-    permutation of axes.
-
-    Parameters
-    ----------
-    image :3d float-valued Pytorch tensor [num_images height width]
-        images to be flipped
-    axis: str in {horizontal, vertical}
-        index of axis to flip
-
-    Returns
-    -------
-    3d float-valued Pytorch tensor [num_images height width]
-        flipped images
-    """
-    if axis == "horizontal":
-        return image[:, :, torch.arange(image.size()[2] - 1, -1, -1)]
-    elif axis == "vertical":
-        return image[:, torch.arange(image.size()[1] - 1, -1, -1), :]
-
-
-def gaussian_blur(image, kernel=(), sigma=(), conv_method='toeplitz'):
-    """
-    Blur 2d image by convolving with Gaussian kernel
-
-    Parameters
-    ----------
-    image: 3d float-valued Pytorch tensor [num_images height width]
-        images to be blurred
-    kernel: 2-tuple of 1d Pytorch tensors, optional
-        separable kernel
-    sigma: 1d float-valued Pytorch tensor with two components (needed if no kernel is provided)
-        standard deviation of Gaussian in each spatial direction (sigma = [sigma_x sigma_y])
-    conv_method: str in {toeplitz, torch, fft}, optional
-        convolution method
-
-    Returns
-    -------
-    3d float-valued Pytorch tensor [num_images heigth width]
-        blurred image
-    """
-    if len(kernel) == 0:
-        kernel = [convolution.gaussian_kernel(sigma_comp) for sigma_comp in sigma]
-    return convolution.conv2d_separable(image, kernel, mode='trunc', method=conv_method)
-
-
-def elastic_deformation(image, vectorfield, int_time, int_method="euler", mode="bilinear", grid=()):
-    """
-    Apply elastic deformation to image by following the flow of a given vectorfield
-    Todo: code contains unnecessary lines specific to the 2d case; this can be easily generalized to
-    arbitrary dimensions.
-
-    Parameters
-    ----------
-    image: 3d float-valued Pytorch tensor [num_images height width]
-        images to be deformed
-    vectorfield: 3d float-valued Pytorch tensor of size [heigth width dim_spat]
-        (Lipschitz) continuous vectorfield
-    int_time: float
-        integration time
-    mode: str in {bilinear, constant}, optional
-        interpolation technique used for evaluating image outside grid
-    grid: float-valued Pytorch-tensor of size [heigth width dim_spat], optional
-        gridpoints {(j, i) : 0 <= j <= n_y - 1, 0 <= i <= n_x - 1}
-
-    Returns
-    -------
-    3d float-valued Pytorch tensor [num_images heigth width]
-        deformed image
-    """
-
-    # Initialization: dimensions
-    dim_spat = 2
-    image_shape = image.size()
-    discret_size = image_shape[len(image_shape) - dim_spat::]
-
-    if len(grid) == 0:
-        grid = compute_grid(discret_size)
-
-    # Flow backwards (Euler). Note: vectorfield[j, i, :] follows standard geometrical ordering
-    if int_method == "euler":
-        deformed_grid = grid - int_time * vectorfield
-    elif int_method == "rk4":
-        raise NotImplementedError("Runga Kutta is not yet implemented")
-
-    # Rescale grid for interpolation
-    deformed_grid = rescale(deformed_grid, torch.tensor([[0, discret_size[1] - 1], [0, discret_size[0] - 1]]),
-                            torch.tensor([[-1, 1], [-1, 1]]))
-
-    # Interpolate image
-    return nnf.grid_sample(image.unsqueeze(0), deformed_grid.unsqueeze_(0), mode=mode).view(*image_shape)
