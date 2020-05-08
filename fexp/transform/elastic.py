131b8420a814041344c27243f8e3853f5fbcb3d6
RaySheombarsing <R.S.S.Sheombarsing@outlook.com>
2020-05-08 23:14:17 +0200

    import torch module

--- fexp/transform/elastic.py
+++ fexp/transform/elastic.py
@@ -1,3 +1,14 @@
+# coding=utf-8
+"""
+Copyright (c) Fexp Contributors
+This source code is licensed under the MIT license found in the
+LICENSE file in the root directory of this source tree.
+"""
+
+import torch
+import torch.nn.functional as nnf
+
+
 def elastic_deformation(image, vectorfield, int_time, int_method="euler", mode="bilinear", grid=()):
     """
     Apply elastic deformation to image by following the flow for small time of a given vectorfield.
